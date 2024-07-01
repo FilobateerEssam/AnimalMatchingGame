bea7ce53dccb5ca90e0736436d581e137b4b29b4
Filobateer Essam <108236186+FilobateerEssam@users.noreply.github.com>
2024-07-01 22:45:18 +0300

    Add project files.

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            AnimalButtons.IsVisible = false;
+
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐵" , "🐵",
+                "🐶" , "🐶",
+                "🐱" , "🐱",
+
+            };
+
+            // To Add Emoji to the buttons
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
